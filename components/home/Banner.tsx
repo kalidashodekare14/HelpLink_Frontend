8de8f3f8797c70f4b58fd467a3e84e86e98f2b26
kalidashodekare14 <kalidashodekare0@gmail.com>
2025-12-08 23:46:07 +0600

    Home page banner section swiper.js implement

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,9 +1,56 @@
 "use client";
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+import { Box } from '@mui/material';
+
 
 const Banner = () => {
     return (
         <div>
-            <h1>This is a banner section</h1>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <Box sx={{
+                        minHeight: "550px",
+                        backgroundImage: "url('/Home/banner1.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}>
+                    </Box>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Box sx={{
+                        minHeight: "550px",
+                        backgroundImage: "url('/Home/banner2.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}>
+                    </Box>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Box sx={{
+                        minHeight: "550px",
+                        backgroundImage: "url('/Home/banner3.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}>
+                    </Box>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
