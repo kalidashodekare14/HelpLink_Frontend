49fa98a3fe92c202cda7cbf2e9d554df9cd84ab4
kalidashodekare14 <kalidashodekare0@gmail.com>
2025-12-10 00:50:36 +0600

    Home page SuccessInfo section completed

--- components/home/MainHome.tsx
+++ components/home/MainHome.tsx
@@ -2,12 +2,14 @@
 
 import Banner from "./banner/Banner";
 import OurMission from "./ourMission/OurMission";
+import SuccessInfo from "./successInfo/SuccessInfo";
 
 const MainHome = () => {
     return (
         <div>
             <Banner />
             <OurMission />
+            <SuccessInfo />
         </div>
     );
 };
---
+++ components/home/successInfo/SuccessInfo.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import { Box, Container, Typography } from "@mui/material";
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import ChatIcon from '@mui/icons-material/Chat';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+
+const SuccessInfo = () => {
+    return (
+        <Container>
+            <Box sx={{
+                display: "grid",
+                gridTemplateColumns: { xs: "1fr 1fr", md: "1fr 1fr 1fr 1fr" },
+                gap: "40px",
+                my: "70px",
+                bgcolor: "#f5f5f5",
+                py: "40px",
+            }}>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "2px",
+                }}>
+                    <HandshakeIcon sx={{
+                        fontSize: "110px",
+                        bgcolor: "#fb8500",
+                        color: "#fff",
+                        p: "30px",
+                        borderRadius: "100%",
+                    }} />
+                    <Typography variant="h5" sx={{
+                        fontSize: "30px",
+                        fontWeight: "600",
+                    }}>
+                        500+
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontSize: "18px", fontWeight: "20" }}  >
+                        Team Members
+                    </Typography>
+                </Box>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "2px",
+                }}>
+                    <ChatIcon sx={{
+                        fontSize: "110px",
+                        bgcolor: "#fb8500",
+                        color: "#fff",
+                        p: "30px",
+                        borderRadius: "100%",
+                    }} />
+                    <Typography variant="h5" sx={{
+                        fontSize: "30px",
+                        fontWeight: "600",
+                    }}>
+                        1200+
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontSize: "18px", fontWeight: "20" }}  >
+                        Client's Review
+                    </Typography>
+                </Box>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "2px",
+                }}>
+                    <EmojiEventsIcon sx={{
+                        fontSize: "110px",
+                        bgcolor: "#fb8500",
+                        color: "#fff",
+                        p: "30px",
+                        borderRadius: "100%",
+                    }} />
+                    <Typography variant="h5" sx={{
+                        fontSize: "30px",
+                        fontWeight: "600",
+                    }}>
+                        100+
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontSize: "18px", fontWeight: "20" }}  >
+                        Winning Awards
+                    </Typography>
+                </Box>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "2px",
+                }}>
+                    <PeopleAltIcon sx={{
+                        fontSize: "110px",
+                        bgcolor: "#fb8500",
+                        color: "#fff",
+                        p: "30px",
+                        borderRadius: "100%",
+                    }} />
+                    <Typography variant="h5" sx={{
+                        fontSize: "30px",
+                        fontWeight: "600",
+                    }}>
+                        100+
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontSize: "18px", fontWeight: "20" }}  >
+                        Happy Clients
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+export default SuccessInfo;
