48a0244b9ab7696ffd507fb79253b62eb3319c98
kalidashodekare14 <kalidashodekare0@gmail.com>
2025-12-31 23:00:26 +0600

    Payment fail and cancel page design update

--- app/payment_cancel/page.tsx
+++ app/payment_cancel/page.tsx
@@ -1,9 +1,33 @@
 "use client"
+import { Container, Stack, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 const payment_cancel = () => {
     return (
         <div className="min-h-screen">
-            <h1>Payment Cancel</h1>
+            <Container maxWidth="lg" sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "500px",
+            }}>
+                <Stack
+                    display={"flex"}
+                    flexDirection={"column"}
+                    justifyItems={"center"}
+                    alignItems={"center"}
+                    gap={"10px"}
+                >
+                    <CloseIcon sx={{
+                        bgcolor: "#e63946",
+                        color: "white",
+                        fontSize: "80px",
+                        borderRadius: "50%"
+                    }} />
+                    <Typography>Payment canceled</Typography>
+                    <Typography>Sorry, your payment has been canceled.</Typography>
+                </Stack>
+            </Container>
         </div>
     );
 };
--- app/payment_fail/page.tsx
+++ app/payment_fail/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+"use client"
+import { Container, Stack, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const payment_fail = () => {
     return (
         <div>
-            <h1>Payment Fail</h1>
+            <Container maxWidth="lg" sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "500px",
+            }}>
+                <Stack
+                    display={"flex"}
+                    flexDirection={"column"}
+                    justifyItems={"center"}
+                    alignItems={"center"}
+                    gap={"10px"}
+                >
+                    <CloseIcon sx={{
+                        bgcolor: "#e63946",
+                        color: "white",
+                        fontSize: "80px",
+                        borderRadius: "50%"
+                    }} />
+                    <Typography>Payment Failed</Typography>
+                    <Typography>Sorry, Your Payment has been Failed.</Typography>
+                </Stack>
+            </Container>
         </div>
     );
 };
