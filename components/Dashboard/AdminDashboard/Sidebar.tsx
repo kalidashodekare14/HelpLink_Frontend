6521ec3d3e96ac0318d399c4236ac9ef57e7ae6d
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-08 18:46:57 +0600

    Admin dashboard sidebar update

--- components/Dashboard/AdminDashboard/Sidebar.tsx
+++ components/Dashboard/AdminDashboard/Sidebar.tsx
@@ -3,6 +3,7 @@ import { Box, Drawer, Stack, Typography, } from "@mui/material";
 import { AdminRoutes } from "@/utils/NavigationRoute";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import HomeIcon from '@mui/icons-material/Home';
 type sidebarProps = {
     isToggle: boolean,
     handleToggleDrawer: (newOpen: boolean) => () => void
@@ -14,39 +15,62 @@ const Sidebar = ({ isToggle, handleToggleDrawer }: sidebarProps) => {
     const pathname = usePathname();
 
     const drawerContent = (
-        <Box sx={{
-            p: 1,
-            bgcolor: "#EBF7F7",
-            height: "100dvh"
-        }}>
-            <Box sx={{ textAlign: 'center' }}>
-                <Typography sx={{ fontSize: "25px", p: 1, }}>HelpLink</Typography>
+        <Stack
+            sx={{
+                p: 1,
+                bgcolor: "#EBF7F7",
+                height: "100dvh"
+            }}
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+        >
+            <Box>
+                <Box sx={{ textAlign: 'center' }}>
+                    <Typography sx={{ fontSize: "25px", p: 1, }}>HelpLink</Typography>
+                </Box>
+                <Stack spacing={1}>
+                    {
+                        AdminRoutes.map(nav => (
+                            <Link key={nav.id} href={nav.path}>
+                                <Box sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: 1,
+                                    p: 1,
+                                    borderRadius: 1,
+                                    cursor: "pointer",
+                                    transition: "0.3s",
+                                    "&:hover": {
+                                        bgcolor: "#C4DDD9",
+                                    },
+                                    bgcolor: pathname === nav.path ? "#C4DDD9" : "inherit"
+                                }}>
+                                    <nav.icon />
+                                    <Typography>{nav.name}</Typography>
+                                </Box>
+                            </Link>
+                        ))
+                    }
+                </Stack>
             </Box>
-            <Stack spacing={1}>
-                {
-                    AdminRoutes.map(nav => (
-                        <Link key={nav.id} href={nav.path}>
-                            <Box sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                gap: 1,
-                                p: 1,
-                                borderRadius: 1,
-                                cursor: "pointer",
-                                transition: "0.3s",
-                                "&:hover": {
-                                    bgcolor: "#C4DDD9",
-                                },
-                                bgcolor: pathname === nav.path ? "#C4DDD9" : "inherit"
-                            }}>
-                                <nav.icon />
-                                <Typography>{nav.name}</Typography>
-                            </Box>
-                        </Link>
-                    ))
-                }
-            </Stack>
-        </Box>
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                p: 1,
+                borderRadius: 1,
+                cursor: "pointer",
+                transition: "0.3s",
+                "&:hover": {
+                    bgcolor: "#C4DDD9",
+                },
+                border: "1px solid #C4DDD9"
+            }}>
+                <HomeIcon />
+                <Typography>Home</Typography>
+            </Box>
+        </Stack>
     );
 
     return (
